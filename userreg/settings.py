14e4bfb1b6ca45aea3c49bae5ee8cad2275a747d
nikhiltelukuntla101 <nikhiltelukuntla999@gmail.com>
2025-05-23 22:59:49 +0530

    Added home view and URL mapping

--- userreg/settings.py
+++ userreg/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-user-reg.onrender.com']
+ALLOWED_HOSTS = ['django-user-reg.onrender.com','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
